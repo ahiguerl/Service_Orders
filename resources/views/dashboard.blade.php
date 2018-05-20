bce4960e8962dbf1ca5a36ba4ba8163bc52d6477
Arcangel <ahiguerl@gmail.com>
2018-05-20 01:42:02 -0500

    sidebar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,17 +5,32 @@
 @endsection
 
 @section('content')
-<div class="container">
+  <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header text-center">Dashboard</div>
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header text-center">Dashboard</div>
+          <div class="card-body row">
 
-                <div class="card-body">
-                    <h1>Ya entra, PERRA!!!!</h1>
-                </div>
+            <div class="sidenav">
+              <div class="">
+                Ordenes de Servicio
+              </div>
+              <div class="">
+
+                <a class="sidenav-item" href="#about">About</a>
+                <a class="sidenav-item" href="#about">About</a>
+                <a class="sidenav-item" href="#about">About</a>
+              </div>
             </div>
+
+            <div class="dashboard-content col">
+              {{-- contenido --}}
+              <h1>Contenido general</h1>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
